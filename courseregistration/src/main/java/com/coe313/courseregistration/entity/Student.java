43228cacfea7f124acba79349f0ff004fdd0d92f
ApparentlyVenus <apparentlyvenus@gmail.com>
2026-03-30 16:03:08 +0300

    forgot to implement student.java, now implemented

--- courseregistration/src/main/java/com/coe313/courseregistration/entity/Student.java
+++ courseregistration/src/main/java/com/coe313/courseregistration/entity/Student.java
@@ -1,5 +1,24 @@
 package com.coe313.courseregistration.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class Student {
-    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer studentID;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User use;
 }
