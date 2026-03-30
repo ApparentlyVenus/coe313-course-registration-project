33c9e983572fcb8b390e4d0e79cff1c699fa40ca
ApparentlyVenus <apparentlyvenus@gmail.com>
2026-03-30 17:04:20 +0300

    added conf package for security authorisation, request filter, and jwt handeling

---
+++ courseregistration/src/main/java/com/coe313/courseregistration/config/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.coe313.courseregistration.config;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+            .setSubject(email)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + expiration))
+            .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+            .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return extractEmail(token).equals(userDetails.getUsername())
+            && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .getExpiration()
+            .before(new Date());
+    }
+}
