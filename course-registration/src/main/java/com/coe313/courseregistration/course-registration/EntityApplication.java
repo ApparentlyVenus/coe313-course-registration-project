160a8254da671cc20c1900cfc63dbe0aa7daade6
ApparentlyVenus <apparentlyvenus@gmail.com>
2026-03-30 12:45:20 +0300

    initial push

---
+++ course-registration/src/main/java/com/coe313/courseregistration/course-registration/EntityApplication.java
@@ -0,0 +1,13 @@
+package com.coe313.courseregistration.course_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityApplication.class, args);
+	}
+
+}
