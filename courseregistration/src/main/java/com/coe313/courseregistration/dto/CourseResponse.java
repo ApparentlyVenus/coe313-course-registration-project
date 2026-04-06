e90b96f80e1527e7a76180490518a0570c426c4a
ApparentlyVenus <apparentlyvenus@gmail.com>
2026-04-07 01:18:23 +0300

    added the course response dto

---
+++ courseregistration/src/main/java/com/coe313/courseregistration/dto/CourseResponse.java
@@ -0,0 +1,18 @@
+package com.coe313.courseregistration.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class CourseResponse {
+    private Integer courseID;
+    private String name;
+    private String abbreviation;
+    private Integer credits;
+    private String departmentName;        
+    private String departmentAbbreviation;
+    private List<String> prerequisites;
+}
